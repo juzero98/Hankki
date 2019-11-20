9843d9c51ef07c048832e1b23dc1d07d3661cd5b
yunji98 <vkfkddlno@naver.com>
2019-11-20 20:44:11 +0900

    SelectActivity button -> MenuActivity    activity_menu (new!)

--- app/src/main/java/com/example/hankki/SelectActivity.kt
+++ app/src/main/java/com/example/hankki/SelectActivity.kt
@@ -1,12 +1,22 @@
 package com.example.hankki
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_select.*
 
 class SelectActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select)
+
+        menuBtn.setOnClickListener{
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
 }
