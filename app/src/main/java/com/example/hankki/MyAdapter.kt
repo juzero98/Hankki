1876389f357f807c982d2394853738b6ed41c9f3
JEONGYUNHA <jhy3990585@naver.com>
2019-11-26 17:49:46 +0900

    update MyPageActivity.kt, MyAdapter.kt, list.xml create MyPage.kt

--- app/src/main/java/com/example/hankki/MyAdapter.kt
+++ app/src/main/java/com/example/hankki/MyAdapter.kt
@@ -1,24 +1,59 @@
 package com.example.hankki
 
+import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.list.view.*
 
-class MyAdapter : BaseAdapter() {
+class MyAdapter : BaseAdapter {
+    private val ctx: Context?
+    private val data: ArrayList<MyPage>
+
+    constructor(_ctx: Context?, _data: ArrayList<MyPage>) {
+        ctx = _ctx
+        data = _data
+    }
+
     override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return data.size
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+       return position.toLong()
     }
 
-    override fun getItem(position: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getItem(position: Int): Any? {
+        return null
     }
+
+
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var view = convertView
+
+        val inflater = LayoutInflater.from(ctx)
+        view = inflater.inflate(R.layout.list, parent, false)
+
+        val menu = view.menu
+        val star = view.star
+        val review = view.review
+        val image = view.img
+
+        val m = data[position]
+
+        Glide.with(view)
+            .load(m.img)
+            .into(image)
+
+        menu.text = m.menu
+        review.text = m.review
+        star.rating = m.star!!
+        return view
     }
 
 
