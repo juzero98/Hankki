c23cda1dfb02f89a6bf8fa1012ec1dbee6fd76e3
yunji98 <vkfkddlno@naver.com>
2019-11-28 19:02:36 +0900

    Board, CommunityActivity, WriteActivity

--- app/src/main/java/com/example/hankki/Board.kt
+++ app/src/main/java/com/example/hankki/Board.kt
@@ -3,5 +3,6 @@ package com.example.hankki
 data class Board(
     var content: String? = null,
     var id: String? = null,
-    var title: String? = null
+    var title: String? = null,
+    var count:Int? = 0
 )
