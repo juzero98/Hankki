807ff6ba9b6353d11b50008c94a75497d3f5a08f
rlaaldMINZI <elen6773@gmail.com>
2019-11-29 21:23:50 +0900

    update MenuFirstFragment,ReviewFirstFragment add OneLineAdapter,OnelineData,list_oneline.xml,oneline_dialog.xml

--- app/src/main/java/com/example/hankki/ReviewFirstFragment.kt
+++ app/src/main/java/com/example/hankki/ReviewFirstFragment.kt
@@ -8,14 +8,23 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.GridView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_menu_firstfragment.*
 import kotlinx.android.synthetic.main.fragment_menu_firstfragment.view.*
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import android.R.attr.fragment
 
-class ReviewFirstFragment : Fragment() {
+
+
+//class ReviewFirstFragment : Fragment() {
+class ReviewFirstFragment : DialogFragment() {
     private val db = FirebaseFirestore.getInstance()
     private val reviewData = ArrayList<SeeReviewData>()
+    private val onelineData = ArrayList<OnelineData>()
+    private val menuData = ArrayList<Menu>()
     private var readSucess  = false
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_review_firstfragment, container, false)!!
@@ -32,12 +41,9 @@ class ReviewFirstFragment : Fragment() {
                     val img = document.get("img").toString()
                     val name = document.get("name").toString()
                     val price = document.get("price").toString()
-
-                            if (!readSucess) {
-                               // reviewData.add(SeeReviewData(img, name, price, star.toFloat()))
-                                reviewData.add(SeeReviewData(img, name, price))
-                            }
-
+                    if (!readSucess) {
+                        reviewData.add(SeeReviewData(img, name, price))
+                    }
                 }
                 upload()
                 readSucess = true
@@ -51,8 +57,58 @@ class ReviewFirstFragment : Fragment() {
         val mGrid = grid
         val mAdapter = ReviewAdapter(this.activity, reviewData)
         mGrid.adapter = mAdapter
+        mGrid.setOnItemClickListener{ parent, view, position, id ->
+            // showDetail(menuData[position].name)
+        }
+    }
+
+    private fun oneLineReview(name : String?) {
+
+        val dialogFragment = fragment as DialogFragment
+        dialogFragment.dismiss()
+
+        val alert: AlertDialog? = null
+        val build = AlertDialog.Builder(this.activity!!)
+        val inflater = activity!!.layoutInflater
+        build.setTitle("One-line review")
+        val dialogLayout = inflater.inflate(R.layout.oneline_dialog, null)
+        // val editText = dialogLayout.findViewById<EditText>(R.id.dialogEt)
+        build.setView(dialogLayout)
+        read1(name)
+        build.setPositiveButton("OK") { dialogInterface, i ->
+
+        }
+        build.show()
 
     }
 
+    private fun read1(menuname : String?) {
+        db.collection("menu")
+            .whereEqualTo("menu", menuname)
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val menuname = document.get("name").toString()
+                    if (!readSucess) {
+                        // reviewData.add(SeeReviewData(img, name, price, star.toFloat()))
+                        onelineData.add(OnelineData(menuname))
+                    }
 
+                }
+                upload1()
+                readSucess = true
+
+            }
+            .addOnFailureListener { exception ->
+                Log.w("", "Error getting documents: ", exception)
+            }
+    }
+
+    fun upload1() {
+        val mGrid = grid
+        val mAdapter =
+            OneLineAdapter(activity!!,onelineData )
+        mGrid.adapter = mAdapter
+
+    }
 }
