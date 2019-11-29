85d682bdc4bb43d13cac6abe7b3bca50400da07b
yunji98 <vkfkddlno@naver.com>
2019-11-29 13:23:20 +0900

    CommunityActivity CommunityAdapter MyCommunity WriteActivity activity_community community_list misc

--- app/src/main/java/com/example/hankki/CommunityActivity.kt
+++ app/src/main/java/com/example/hankki/CommunityActivity.kt
@@ -2,21 +2,23 @@ package com.example.hankki
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_community.*
 
 
-
-
 class CommunityActivity : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
+    private val commuData = ArrayList<MyCommunity>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_community)
 
         val id = intent.getStringExtra("id")
-        val db = FirebaseFirestore.getInstance()
 
+        readFirestore()
 
         writeBtn.setOnClickListener{
             val intent = Intent(this, WriteActivity::class.java)
@@ -29,8 +31,28 @@ class CommunityActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
 
+    fun readFirestore(){ //db 읽어와
+        db.collection("board")
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents ) {
+                    var title = document.get("title").toString()
+                    var content = document.get("content").toString()
 
+                    commuData.add(MyCommunity(title, content))
+                }
+                upload()
+            }
+            .addOnFailureListener { exception ->
+                Log.w("", "Error getting documents: ", exception)
+            }
+    }
 
+    fun upload(){ //gridview에 upload
+        val mGrid = grid
+        val mAdapter = CommunityAdapter(this, commuData)
+        mGrid.adapter = mAdapter
     }
 }
---
+++ app/src/main/java/com/example/hankki/CommunityAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.hankki
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.community_list.view.*
+
+class CommunityAdapter : BaseAdapter {
+
+    private val ctx: Context?
+    private val data: ArrayList<MyCommunity>
+
+    constructor(_ctx: Context?, _data: ArrayList<MyCommunity>) {
+        ctx = _ctx
+        data = _data
+    }
+
+    override fun getCount(): Int {
+        return data.size
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getItem(position: Int): Any? {
+        return null
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var view = convertView
+
+        val inflater = LayoutInflater.from(ctx)
+        view = inflater.inflate(R.layout.community_list, parent, false)
+
+        val titleId = view.titleId
+        val contentId = view.contentId
+
+        val m = data[position]
+
+        titleId.text = m.titleId
+        contentId.text = m.contentId
+
+        return view
+    }
+}
